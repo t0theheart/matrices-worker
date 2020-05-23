cddba85e4651538afdf6231f1832147c2da931d0
t0theheart <35728736+t0theheart@users.noreply.github.com>
2020-05-23 15:58:28 +0300

    create class MatricesGeneratorAdapter. It is the MatricesGenerator's adapter class to used by the client

---
+++ matrices_workers/matrices_adapters/matrices_generator_adapter.py
@@ -0,0 +1,6 @@
+from matrices_workers.matrices_worker.matrices_worker import MatricesWorker
+from matrices_workers.matrices_generator.matrices_generator import MatricesGenerator
+
+
+class MatricesGeneratorAdapter(MatricesWorker, MatricesGenerator):
+    """text"""
